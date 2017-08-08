ac233957cd0b68ef387dde3c934131ad17ab8dc4
Rotem Cohen <rotembcohen@gmail.com>
2017-08-08 18:06:57 -0400

    buy in functionality

--- containers/GameView.js
+++ containers/GameView.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet, Text, View, ScrollView, FlatList, Button, AsyncStorage,
+  StyleSheet, Text, View, ScrollView, FlatList, Button, AsyncStorage, TextInput,
 } from 'react-native';
 
 import styles from '../Styles';
+import * as utils from '../UtilFunctions';
 
 export default class GameView extends Component {
 
@@ -12,6 +13,8 @@ export default class GameView extends Component {
 		this.state = {
 			token: 'no token',
 			playerList: [],
+			game_identifier: 'no identifier',
+			buy_in_amount: 0,
 		};
 	}
 
@@ -24,7 +27,26 @@ export default class GameView extends Component {
 		this.setState({
 			token: token,
 			playerList: game.bets,
-		})
+			game_identifier: game.identifier,
+		});
+	}
+
+	//TODO: add way to push updates/ or check updates frequently
+
+	async buy_in(){
+		const gameObj = await utils.fetchFromServer('games/' + this.state.game_identifier + "/buy_in/",'POST',{
+			amount: parseInt(this.state.buy_in_amount)
+		},this.state.token);
+		if (gameObj.status === 200){
+			gameString = gameObj._bodyText;
+
+			await AsyncStorage.setItem('@pokerBuddy:currentGame', gameString);
+			
+			let game = JSON.parse(gameString);
+			this.setState({
+				playerList: game.bets,
+			});
+		}
 	}
 
 	render() {
@@ -36,8 +58,15 @@ export default class GameView extends Component {
 	            data={this.state.playerList}
 	            renderItem={({item}) => <Text>BI LG {item.amount} {item.player}</Text>}
 	        />
-	        <Button title='Buy In' onPress={()=>{}} />
-	        <Button title='Leave Game' onPress={()=>{navigation.navigate('PayView')}} />
+	        <View>
+		        <TextInput
+		      		style={styles.textinput}
+		      		onChangeText={(text)=>{this.setState({buy_in_amount:text})}}
+		      		value={this.state.buy_in_amount.toString()}
+	      		/>
+		        <Button title='Buy In' onPress={()=>{this.buy_in()}} />
+		        <Button title='Leave Game' onPress={()=>{navigation.navigate('PayView')}} />
+	        </View>
 	      </ScrollView>
 	    );
 	}
