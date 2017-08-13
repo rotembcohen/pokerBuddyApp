aced1f8c45cc42e476210da27414e7538a6b5d0c
Rotem Cohen <rotem@timeflash.com>
2017-08-13 10:39:38 -0400

    create game view removal

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { StackNavigator } from 'react-navigation';
 import WelcomeView from './containers/WelcomeView';
 import LoginView from './containers/LoginView';
 import HomeView from './containers/HomeView';
-import CreateGameView from './containers/CreateGameView';
 import GameView from './containers/GameView';
 import ModalExample from './modal-example/app';
 import RegistrationView from './containers/RegistrationView';
@@ -13,7 +12,6 @@ const App = StackNavigator({
     WelcomeView: {screen: WelcomeView},
     LoginView: {screen: LoginView},
     HomeView: {screen: HomeView},
-    CreateGameView: {screen: CreateGameView},
     GameView: {screen: GameView},
     ModalExample: {screen: ModalExample},
     RegistrationView: {screen: RegistrationView}
--- containers/HomeView.js
+++ containers/HomeView.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet, Text, View, ScrollView, FlatList, Button, TextInput, AsyncStorage, Picker
+  StyleSheet, Text, View, ScrollView, FlatList, Button, TextInput, AsyncStorage, Picker, TouchableOpacity
 } from 'react-native';
 
+import Modal from 'react-native-modal';
 import styles from '../Styles';
 import * as utils from '../UtilFunctions';
 
 export default class HomeView extends Component {
 
 	static navigationOptions = {
-		title: 'Choose Game'
+		title: 'pocAt v0.1'
 	}
 
 	constructor(props){
@@ -21,10 +22,68 @@ export default class HomeView extends Component {
 			user: navigation.state.params.user,
 			game_identifier: '',
 			active_games: [],
+			isModalVisible: false,
+			min_bet: 20,
 		};
 		this.getActiveGames();
 	}
 	
+	async createGame(navigation){
+		//TODO: handle network error
+		if (this.state.min_bet === '' || this.state.token === '' || this.state.token === 'no token'){
+			return;
+		}
+		const response = await utils.fetchFromServer('games/','POST',{
+			min_bet: this.state.min_bet
+		},this.state.token);
+
+		console.log(response.status);
+		//game created succesfully
+		if (response.status===201){
+			//get new game identifier
+			let game_json = JSON.parse(response._bodyText);
+			let game_identifier = game_json.identifier;
+			
+			//TODO: add option for host not to immediately join game
+			//join game
+			game = await utils.joinGame(game_identifier,this.state.token,this.state.user);
+			this.setState({isModalVisible:false});
+			navigation.navigate('GameView',{game: game,user: this.state.user,token:this.state.token});
+		}
+		//TODO:else
+		
+	}
+
+	//TODO: put in utils
+	_renderButton = (text, onPress) => (
+		<TouchableOpacity onPress={onPress}>
+			<View style={styles.modalButton}>
+				<Text>{text}</Text>
+			</View>
+		</TouchableOpacity>
+	);
+
+	_renderModalContent = () => {
+		navigation = this.state.navigation;
+	    return (
+	      <View style={styles.modalContent}>
+	      	<Text style={styles.textSubheader}>Starting bet</Text>
+	      	<TextInput
+	      		style={styles.textinput}
+	      		onChangeText={(text)=>{this.setState({min_bet:text})}}
+	      		value={this.state.min_bet.toString()}
+	      		selectTextOnFocus={true}
+	      		autoFocus={true}
+	      		onSubmitEditing={()=>{this.createGame(navigation)}}
+      		/>
+	        <View style={{flexDirection:'row'}}>
+				{this._renderButton('Cancel', ()=> this.setState({isModalVisible:false}))}
+				{this._renderButton('Create Game', ()=>{this.createGame(navigation)})}
+			</View>
+	      </View>
+	    );
+	}
+
 	async getActiveGames(){
 		const response = await utils.fetchFromServer(
 			'users/' + this.state.user.id + '/active_games/',
@@ -49,14 +108,18 @@ export default class HomeView extends Component {
 		let active_games = this.state.active_games;
 		return (
 	      <View style={styles.container}>
-	      	<Button title='Create Game' onPress={()=>{navigation.navigate('CreateGameView',{token:this.state.token,user:this.state.user})}} />
-	      	<Text>Current User: {this.state.user.username}</Text>
+	      	<Modal isVisible={this.state.isModalVisible === true}>
+				{this._renderModalContent()}
+	        </Modal>
+	      	<Button title='Create Game' onPress={()=>{this.setState({isModalVisible:true})}} />
+	      	<Text>Current User: {this.state.user.first_name + " " + this.state.user.last_name}</Text>
 	      	<Text>Game Address: </Text>
 	      	<TextInput
 	      		style={styles.textinput}
 	      		onChangeText={(text)=>{this.setState({game_identifier:text})}}
 	      		value={this.state.game_identifier}
 	      		autoCapitalize={'characters'}
+	      		selectTextOnFocus={true}
 	      		maxLength={5}
       		/>
 	        <Button title='Join Game' onPress={async ()=>{
@@ -79,7 +142,3 @@ export default class HomeView extends Component {
 	}
 
 }
-
-
-//believe or not this is possible:
-//<Button title='Create Game' onPress={()=>{navigation.navigate('CreateGameView',currentState)}} />
