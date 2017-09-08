4788ecd8a66a47064118d4a44c534ded9635f556
Rotem Cohen <rotembcohen@gmail.com>
2017-09-08 19:16:58 -0400

    more tips

--- components/TipsDatabase.js
+++ components/TipsDatabase.js
@@ -1,8 +1,17 @@
 const TipsDatabase = [
-    "The chance of one of your cards making a pair on the flop is about a third",
-    "By the river, your chances of making a pair go up to roughly a half",
+    "The chance of one of your cards making a pair on the flop is about 1/3",
+    "By the river, your chances of making a pair go up to roughly 1/2",
     "Don't play any two cards just because they're suited. It only improves your hand by 2.5%",
-    "If you've got one card short of a full flush after the flop, you'll make your hand a third of the time",
-    "The probability of flopping two-pair (from non-paired hole cards) is about 2%"
+    "If you've got one card short of a full flush after the flop, you'll make your hand a 1/3 of the time",
+    "The probability of flopping two-pair (from non-paired hole cards) is about 2%",
+    "Every out you have on the flop equals 4% for a winning hand on the turn or river",
+    "After the turn, multiply the number of outs by 2 to give chance of winning on the river",
+    "If you have a pocket pair, you will flop a set roughly one time in 9",
+    "If you have two suited cards, you will flop a flush roughly one time in 118",
+    "On average, you'll be dealt pocket As once every 220 hands",
+    "Straight flush is likely to happen once in 64,974 hands",
+    "There are 169 non-equivalent starting hands, not all of them equally likely",
+    "About every 6th flop will have a pair",
+    "Against any individual random hand, AA will win 85% of the time"
 ];
 export default TipsDatabase;
