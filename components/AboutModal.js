a69e4d3db1b86a03aa2f2a92c2cc2b2293ade362
Rotem Cohen <rotembcohen@gmail.com>
2017-09-14 19:35:48 -0400

    more details in about modal

--- components/AboutModal.js
+++ components/AboutModal.js
@@ -17,10 +17,18 @@ export default class AboutModal extends Component {
 				<Text style={styles.home_boldLink} onPress={
 					()=>Linking.openURL('mailto:hecodesthings@gmail.com?subject=Pocat v'+APP_VERSION)
 				}>hecodesthings@gmail.com</Text>
+				
+				<Text style={{marginTop:10,textAlign:'center'}}>
+					<Text style={styles.boldText}>Pocat</Text> makes it easy and fun to play casual poker with friends.{"\n\n"}
+					Easily manage buy-ins, cashing out and who needs to pay who.{"\n\n"}
+					This app should be used at your own risk and only where casual gambling is legal.{"\n\n"}
+				</Text>
+				
 				<View style={styles.game_modals_aboutButtonsContainer}>
 	      			{donateButton}
 	      			<IconButton action={this.props.onClose} name="ios-close-circle-outline" text="Close" />
 		        </View>
+
 			</View>
 		);
 	}
